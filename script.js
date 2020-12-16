97f19c207d4bc8eaefd3a989b6c70dc243746762
ramirobelmonte <ramirobelmon@gmail.com>
2020-12-16 18:13:37 -0300

    beta

--- script.js
+++ script.js
@@ -1,43 +1,22 @@
-window.onload = function() {
+window.onload = () => {
   const xhttp = new XMLHttpRequest();
   xhttp.open('GET', 'https://script.google.com/macros/s/AKfycbxgSvcs5gANRET0tFDdBl1Hh5dlekwkXMnamwIYAPjr3yEXRj8/exec', true);
   xhttp.send();
-
-  xhttp.onreadystatechange = function() {
-
-    if (this.readyState == 4 && this.status == 200) {
-
-      let datos = JSON.parse(this.responseText);
-      const formulario = document.querySelector('#formulario');
+  xhttp.onload = function(){
+      let objetosJson = JSON.parse(this.responseText);
+      const searchInput = document.querySelector('#searchInput');
       const tabla = document.querySelector('#tabla');
 
-      const filtrar = ()=> {
-        const texto = formulario.value.toLowerCase().replace(" ","");
-        tabla.innerHTML = '';
-
-        for (var i = 0; i < datos.length; i++){
-            let item = datos[i];
-            let dato = datos[i].Nombre.toLowerCase().replace(" ","");
-
-            if (dato.indexOf(texto) !== -1) {
-
-              tabla.innerHTML += `
-              <tr>
-                <td class="icono">UNDEFINED</td>
-                <td class="nombre">
-                  <a class="tarjeta" href='${item.Link}'>
-                    ${item.Nombre.replace(/\w[^_]+$/g,"")}
-                  </a>
-                </td>
-                <td class="etiqueta">${item.Nombre.match(/[^\_]+(?=\.)/g)}</td>
-              </tr>`
-            }
-        }
-      }
-
-      formulario.addEventListener('keyup', filtrar)
-      filtrar()
-
-    }
-  }
+      let fragment = document.createDocumentFragment();
+      let templateTr = document.querySelector('#template-tr').content;
+        for(let objetoJson of objetosJson){
+          templateTr.querySelector('.tipo').textContent = objetoJson.tipo;
+          templateTr.querySelector('.nombre a').setAttribute("href", objetoJson.Link);
+          templateTr.querySelector('.nombre a').textContent = objetoJson.Nombre;
+          templateTr.querySelector('.asignatura').textContent = objetoJson.Asignatura;
+          const clone = templateTr.cloneNode(true);
+          fragment.appendChild(clone);
+        };
+        tabla.appendChild(fragment);
+      };
 };
